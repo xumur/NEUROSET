f37bf05fbef2194fa9dbeff1e0e5a5426291ca05
xumur <nanaimo0555@mail.ru>
2023-02-25 17:16:26 +0300

    Initital commit

--- fit.py
+++ fit.py
@@ -7,10 +7,11 @@ from tensorflow.keras.utils import to_categorical
 import numpy as np
 import os.path
 
+# Проверка на наличие файла модели (если нет, то создаёт новую модель)
 if os.path.isfile('my_model.h5'):
     model = keras.models.load_model('my_model.h5')
 else:
-    # Define the model architecture
+    # Определение архитектуры модели
     model = Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
     model.add(MaxPooling2D((2, 2)))
@@ -21,18 +22,18 @@ else:
     model.add(Dense(64, activation='relu'))
     model.add(Dense(10, activation='softmax'))
 
-    # Compile the model
+    # Компилирование модели
     model.compile(optimizer=SGD(lr=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Load and preprocess the data
+# Загрузка и предварительная обработка данных
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 y_train = to_categorical(y_train, num_classes=10)
 y_test = to_categorical(y_test, num_classes=10)
 
-# Train the model
+# Обучение модели
 model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
 
-# Save the model to file
+# Сохранение модели в файл
 model.save('my_model.h5')
